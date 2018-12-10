e7453942d0c819e7c864a3c008c198ed07a4e662
YetAnotherIgor <YetAnotherIgor@users.noreply.github.com>
2018-12-10 12:44:28 -0800

    Refactored rst section hierarchy in src code

--- {{ cookiecutter.project_pkg_name }}/{{ cookiecutter.project_module_name }}/__init__.py
+++ {{ cookiecutter.project_pkg_name }}/{{ cookiecutter.project_module_name }}/__init__.py
@@ -1,6 +1,6 @@
 """
 {{ cookiecutter.project_module_name }}
---------------------------------------
+======================================
 
-Root module of the {{ cookiecutter.project_pkg_name }} package.
+Root module of the ``{{ cookiecutter.project_pkg_name }}`` package.
 """
